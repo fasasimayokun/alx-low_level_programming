e6f2c312b84c41e292fb4084b23e2a8e65c0d763
fasasimayokun <maykay.fash@gmail.com>
2023-07-13 11:12:09 -0700

    my commit for 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a function that prints with the puts function
+ * Return: it returns 0 which indicates a successful program
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
