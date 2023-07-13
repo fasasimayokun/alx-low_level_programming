2bc6764aac526091c194d94258cdf45ab1abdeb7
fasasimayokun <maykay.fash@gmail.com>
2023-07-13 12:13:35 -0700

    my commit for 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main - a function that outputs a string to the standard error on unix
- * Return: 0 successful program
+ * Return: 1 successful program
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
