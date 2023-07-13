71caca2257e44d4979540bda0b1f96cdef0fd5ab
fasasimayokun <maykay.fash@gmail.com>
2023-07-13 11:19:48 -0700

    my commit for 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function that outputs with the prinf function
+ * Return: it returns 0 indicating a successfull program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
