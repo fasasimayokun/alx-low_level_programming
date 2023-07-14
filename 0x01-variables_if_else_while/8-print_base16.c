e577c51e47b0967a9a57d178e86ade96608cc94b
fasasimayokun <maykay.fash@gmail.com>
2023-07-14 01:28:46 -0700

    my commit for 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints 0-9 and all hexadecimal numbers using putchar function
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
