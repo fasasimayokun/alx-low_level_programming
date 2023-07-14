363271c5bacb59440773453fb29ad5ef7c1c8d6b
fasasimayokun <maykay.fash@gmail.com>
2023-07-14 00:54:15 -0700

    my commit for 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the upper and lower case of alphabet
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar("\n");
+	return (0);
+}
