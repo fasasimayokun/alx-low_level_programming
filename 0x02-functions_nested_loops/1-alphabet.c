7516d790353990d4bf3d4eaa16d648be2ebd8a86
fasasimayokun <maykay.fash@gmail.com>
2023-07-18 11:18:55 -0700

    my commit for 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include 'main.h'
+/**
+ * print_alphabet - a function that prints lowercase
+ * alphabets
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	int alpha = 97;
+
+	while (alpha < 123)
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
