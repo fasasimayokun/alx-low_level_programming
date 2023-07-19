221b1f78818a425ff124b03c0612ec23c65b820d
fasasimayokun <maykay.fash@gmail.com>
2023-07-19 04:54:02 -0700

    my commit for 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - a function that adds 2 integer
+ * @x: first integer argument
+ * @y: second integer argument
+ * Return: the result
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
