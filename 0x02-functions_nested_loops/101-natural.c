81313addd0252eaa18d08763bcbbfe7341b089ad
fasasimayokun <maykay.fash@gmail.com>
2023-07-19 07:05:45 -0700

    my commit for 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of all mutliples of 3 & 5 to 1024
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 0;
+	int sum = 0;
+
+	while (a < 1024)
+	{
+		if ((a % 3 == 0) || (a % 5 == 0))
+		{
+			sume += a;
+		}
+		a++;
+	}
+	printf("%d/n", sum);
+	return (0);
+}
