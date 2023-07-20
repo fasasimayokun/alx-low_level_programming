86f9a1a6edac63fe3862a00c05722e8cb0fce7bd
fasasimayokun <maykay.fash@gmail.com>
2023-07-20 15:18:10 -0700

    my commit for 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prime factors of 1231952 are 2, 2, 2, 2, 37 and 2081
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int num;
+	long int large;
+	long int i;
+
+	num = 612852475143;
+	large = -1;
+
+	while (num % 2 == 0)
+	{
+		large = 2;
+		num = num / 2;
+	}
+
+	for (i = 3; i <= sqrt(num); i = i + 2)
+	{
+		while (num % i == 0)
+		{
+			large = i;
+			num = num / i;
+		}
+	}
+
+	if (num > 2)
+		large = num;
+	printf("%ld\n", large);
+
+	return (0);
+}
