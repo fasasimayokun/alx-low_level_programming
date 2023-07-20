d53cd4adcc9f8ca24eb074d5165ac37b9421d4fe
fasasimayokun <maykay.fash@gmail.com>
2023-07-20 15:06:10 -0700

    my commit for 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,13 +13,13 @@ void print_triangle(int size)
 	{
 		int i, j;
 
-		for (int i = 1; i <= size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (int j = i; j > size; j++)
+			for (j = i; j > size; j++)
 			{
 				_putchar(' ');
 			}
-			for (int j = 1; j <= i; j++)
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
