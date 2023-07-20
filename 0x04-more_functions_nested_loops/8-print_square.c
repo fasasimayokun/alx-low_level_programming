bf9401ac1412c01d064cbedd2f992fd24ef9d68a
fasasimayokun <maykay.fash@gmail.com>
2023-07-20 14:27:44 -0700

    my commit for 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_square - a function that prints a square
+ * @size: int values
+ * Return: void
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i = 0;
+
+		while (i < size)
+		{
+			int j = 0;
+
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+		}
+	}
+}
