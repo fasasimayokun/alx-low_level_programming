b965de95c960a2ee8db98af9635a26c91efee06b
fasasimayokun <maykay.fash@gmail.com>
2023-07-24 08:37:22 -0700

    my commit for 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,16 @@
 /**
  * _strlen - a function that returns the length of a string
  * @s: the string argument
- * Return: void
+ * Return: length of string
  */
 int _strlen(char *s)
 {
 	int count = 0;
-	
+
 	while (*s != '\0')
 	{
 		count += 1;
 		s++;
 	}
-	return count;
+	return (count);
 }
