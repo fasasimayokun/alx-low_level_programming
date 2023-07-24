8c5e2c0fb43f963b13545133379a548b460e23b5
fasasimayokun <maykay.fash@gmail.com>
2023-07-24 09:22:54 -0700

    my commit for 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - a function that reverses a string
+ * @s: a string argument
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char temp;
+	int i;
+	int str_len = 0;
+
+	while (*s != '\0')
+	{
+		str_len += 1;
+		s++;
+	}
+
+	for (i = 0; i < str_len; i++)
+	{
+		str_len--;
+		temp  = s[i];
+		s[i] = s[str_len];
+		s[str_len] = temp;
+	}
+}
