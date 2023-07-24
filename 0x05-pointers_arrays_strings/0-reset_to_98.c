2af6927983afa1a6c5a3fdedfbe79f22aa62bd42
fasasimayokun <maykay.fash@gmail.com>
2023-07-24 08:12:36 -0700

    my commit for 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - a function that takes a pointer to an int
+ * as parameter and updates the value it points to to 98
+ * @n: a pointer argument
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
