b9418f4b47960f04612095e9bbdcafc4bf92dccb
fasasimayokun <maykay.fash@gmail.com>
2023-07-29 12:21:40 -0700

    my commit for 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,4 +9,23 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int strlen_dest;
+	int strlen_src;
+
+	strlen_dest = 0;
+	while (dest[strlen_dest] != '\0')
+	{
+		strlen_dest++;
+	}
+
+	strlen_src = 0;
+
+	while (src[strlen_src] != '\0' && strlen_src < n)
+	{
+		dest[strlen_dest] = src[strlen_src];
+		strlen_dest++;
+		strlen_src++;
+	}
+	dest[strlen_dest] = '\0';
+	return (dest);
 }
