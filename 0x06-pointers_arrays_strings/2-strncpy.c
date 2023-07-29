b526eaa1b65c29fae42bb3182a236f7148d981fa
fasasimayokun <maykay.fash@gmail.com>
2023-07-29 12:27:40 -0700

    my commit for 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,4 +9,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	int strlen = 0;
+
+	while (strlen < n && src[strlen] != '\0')
+	{
+		dest[strlen] = src[strlen];
+		strlen++;
+	}
+	while (strlen < n)
+	{
+		dest[strlen] = '\0';
+		strlen++;
+	}
+	return (dest);
 }
