4b72ba181f6640a849fb5b4b86e61e238384b40c
fasasimayokun <maykay.fash@gmail.com>
2023-07-29 12:39:55 -0700

    my commit for 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,4 +7,15 @@
  */
 char *string_toupper(char *str)
 {
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (str[len] >= 97 && str[len] <= 122)
+		{
+			str[len] = str[len] - 32;
+		}
+		len++;
+	}
+	return (str);
 }
