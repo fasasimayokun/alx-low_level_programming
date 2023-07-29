57adfaacf567e831ccd5799e0c34a9b6fe72a499
fasasimayokun <maykay.fash@gmail.com>
2023-07-29 12:50:35 -0700

    my commit for 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,4 +6,30 @@
  */
 char *cap_string(char *word)
 {
+	int num;
+
+	num = 0;
+
+	while (word[num])
+	{
+		while (!(word[num] >= 97 && word[num] <= 122))
+			num++;
+		if (word[num - 1] == ' ' ||
+		    word[num - 1] == '\t' ||
+		    word[num - 1] == '\n' ||
+		    word[num - 1] == ',' ||
+		    word[num - 1] == '.' ||
+		    word[num - 1] == ';' ||
+		    word[num - 1] == '!' ||
+		    word[num - 1] == '?' ||
+		    word[num - 1] == '"' ||
+		    word[num - 1] == '(' ||
+		    word[num - 1] == ')' ||
+		    word[num - 1] == '{' ||
+		    word[num - 1] == '}' ||
+		    num == 0)
+			word[num] = word[num] - 32;
+		num++;
+	}
+	return (word);
 }
