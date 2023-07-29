060de766602642fe79e3235c4a53d9963762fb03
fasasimayokun <maykay.fash@gmail.com>
2023-07-29 12:31:34 -0700

    my commit for 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,4 +7,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
+	int strlen = 0;
+
+	while (s1[strlen] != '\0' && s2[strlen] != '\0')
+	{
+		if (s1[strlen] != s2[strlen])
+		{
+			return (s1[strlen] - s2[strlen]);
+		}
+		strlen++;
+	}
+	return (0);
 }
