ad739c1fe97f23c0779de2c6f245c23fa9fb2547
fasasimayokun <maykay.fash@gmail.com>
2023-07-31 10:21:42 -0700

    my commit for 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr -  function that locates a character in a string
+ * @s: string input
+ * @c: char to find
+ * Return: a pointer to the 1st occurence of char
+ */
+char *_strchr(char *s, char c)
+{
+	int num = 0;
+
+	while (s[num] != '\0')
+	{
+		if (s[num] == c)
+		{
+			return (&s[num]);
+		}
+		num++;
+	}
+	return (0);
+}
