e04202dd7a7900c1f2b9b0bf6c1016eebc2bb3c7
fasasimayokun <maykay.fash@gmail.com>
2023-08-02 11:19:06 -0700

    my commit for 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - a function that returns the natural squrt of a num
+ * @n: the input valu to find the sqrt of
+ * Return: the result
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (_real_sqrt_recursion(n, 0));
+}
+/**
+ * _real_sqrt_recursion - recurse to find the natural
+ * sqrt of the number
+ * @n: num to find the sqrt
+ * @it: itrator
+ * Return: the result
+ */
+int _real_sqrt_recursion(int n, int it)
+{
+	if (it * it > n)
+	{
+		return (-1);
+	}
+	else if (it * it == n)
+	{
+		return (it);
+	}
+	return (_real_sqrt_recursion(n, it + 1));
+}
