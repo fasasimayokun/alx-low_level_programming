6335c2661267f615a6c1538a1bc175ea0d40b8a9
fasasimayokun <maykay.fash@gmail.com>
2023-08-02 11:43:19 -0700

    my commit for 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * is_prime_number - a function that returns 1 if the input
+ * integer is a prime number, otherwise return 0.
+ * @n: input val
+ * Return: int val
+ */
+int _check_prime(int n, int it);
+
+int is_prime_number(int n)
+{
+	return (_check_prime(n, 1));
+}
+/**
+ * _check_prime - check for prime num
+ * @n: num to be checked
+ * @it: the iteration number
+ * Return: int val
+ */
+int _check_prime(int n, int it)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n % it == 0 && it > 1)
+	{
+		return (0);
+	}
+	else if ((n / it) < it)
+	{
+		return (1);
+	}
+	return (_check_prime(n, it + 1));
+}
