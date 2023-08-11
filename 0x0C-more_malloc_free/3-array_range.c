9d551a3b3c4c0e99a7427b4a65ac7ce5bd9bc7f1
fasasimayokun <maykay.fash@gmail.com>
2023-08-11 08:10:13 -0700

    my commit for 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,3 +7,24 @@
  */
 int *array_range(int min, int max)
 {
+	int nm, len;
+	int *pt;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min + 1;
+
+	pt = malloc(sizeof(int) * len);
+
+	if (pt == NULL)
+	{
+		return (NULL);
+	}
+
+	for (nm = 0; min <= max; nm++)
+	{
+		pt[nm] = min++;
+	}
+	return (pt);
+}
