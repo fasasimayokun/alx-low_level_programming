5f2d381f25cc6337e486c6192a0e2ccde50d67be
fasasimayokun <maykay.fash@gmail.com>
2023-08-15 13:55:31 -0700

    my commit for 3_dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,15 @@
  * @owner: owner of dog
  * Return: nothing (void)
  */
-struct dog
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif
