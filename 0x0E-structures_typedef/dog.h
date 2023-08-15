b8eba3f3c703db0aa9cb834611de3210316ced56
fasasimayokun <maykay.fash@gmail.com>
2023-08-15 10:57:34 -0700

    my commit for dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * val_dog - initialize a variable of type struct dog
+ * init_dog - initialize a variable of type struct dog
  * @d: address to struct dog
  * @name: name to input
  * @age: age to input
  * @owner: owner of dog
  * Return: nothing (void)
  */
-void val_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		d = malloc(sizeof(struct dog));
