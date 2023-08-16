20cf30633d9d78598c643888f89a631e55ad3920
fasasimayokun <maykay.fash@gmail.com>
2023-08-16 11:05:50 -0700

    my commit for 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name -  outputs name using function pointer
+ * @name: str name
+ * @f: function pointer
+ * Return: nothing (void)
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+my readme for task 0x0F-function_pointers
