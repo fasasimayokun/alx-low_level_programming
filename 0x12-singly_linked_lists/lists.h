c649c56700b55c8bd5249d67fc4f4d15ecf90998
fasasimayokun <maykay.fash@gmail.com>
2023-08-24 12:06:41 -0700

    my commit for 0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,12 +7,16 @@
  * @len: str length
  * @next: pointing to the next node
  */
-typedef struct list_s
+struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_t;
+};
+/**
+ * list_t - new struct for list_s
+ */
+typedef struct list_s list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
