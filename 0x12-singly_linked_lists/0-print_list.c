9e154b91d4e8ef8b338a8de77569a6c04db7d5fa
fasasimayokun <maykay.fash@gmail.com>
2023-08-25 09:48:25 -0700

    my commit for 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t nm = 0;
 
 	while (h)
 	{
@@ -21,7 +21,7 @@ size_t print_list(const list_t *h)
 			printf("[%u] %s\n", h->len, h->str);
 		}
 		h = h->next;
-		s++;
+		nm++;
 	}
-	return (s);
+	return (nm);
 }
