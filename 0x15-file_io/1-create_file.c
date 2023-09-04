9426a9a885ad6fdd567d1b027a8ac943a115b5a1
fasasimayokun <maykay.fash@gmail.com>
2023-09-04 06:23:20 -0700

    my commit for 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content)
 	{
-		for (cnt = 0; text_content[cnt]; cnt)
+		for (cnt = 0; text_content[cnt];)
 		{
 			cnt++;
 		}
